e5dc59d2ac777377d818c16d67bf323167b6a7f9
jonas <saffarjonas@gmail.com>
2024-09-13 18:29:52 +0300

    Added raylib dependency

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+#include "raylib.h"
+
 int main(void) {
     std::cout << "Hello, World!\n";
+    std::cout << TextFormat("raylib version: %d.%d.%d\n", RAYLIB_VERSION_MAJOR, RAYLIB_VERSION_MINOR, RAYLIB_VERSION_PATCH);
     return 0;
 }
